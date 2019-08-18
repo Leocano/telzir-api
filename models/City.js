fa7c806aec4a66b3ad32a46860965d7c76322bad
Leonardo Cano <leocano4242@gmail.com>
2019-08-18 15:05:29 -0300

    Fix foreign key in City model

--- models/City.js
+++ models/City.js
@@ -1,10 +1,15 @@
 import Sequelize from 'sequelize'
 import conn from '../db/conn'
+import DDDCode from './DDDCode'
 
 const City = conn.define('city', {
   ddd_code: {
     type: Sequelize.INTEGER,
-    allowNull: false
+    allowNull: false,
+    references: {
+      model: DDDCode,
+      key: 'ddd_code'
+    }
   },
   name: {
     type: Sequelize.STRING,
