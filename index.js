5dfe4326573d7232ae00b38a7600a8b878460e25
Leonardo Cano <leocano4242@gmail.com>
2019-08-18 13:55:43 -0300

    Remove comments

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// import express from 'express'
-// import router from './routes/router'
 import app from './app'
 
 const port = process.env.port || 8000
-
-// const app = express()
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-// app.use('/', router)
 
 app.listen(port, () => {
   console.log('Server running on port ' + port)
